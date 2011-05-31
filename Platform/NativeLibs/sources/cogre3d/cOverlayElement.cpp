780489a7b75269721dc7c6f69e88ea0c17cd01a3
Daniel Alves <danfma@gmail.com>
2011-05-30 21:54:42 -0300

    - Platform configuration

--- Platform/NativeLibs/sources/cogre3d/cOverlayElement.cpp
+++ Platform/NativeLibs/sources/cogre3d/cOverlayElement.cpp
@@ -1,15 +1,22 @@
 #include "cOgre.h"
 
-INV_EXPORT _wstring
+INV_EXPORT _string
 INV_CALL overlayelement_get_caption(InvHandle self)
 {
-	Ogre::DisplayString text = asOverlayElement(self)->getCaption();
+	_string text = const_cast<_string>(asOverlayElement(self)->getCaption().asUTF8_c_str());
 
-	return copyString(text.asWStr());
+	int length = strlen(text);
+	_string textCopy = new _char[length];
+
+	for (int i = 0; i < length; i++) {
+		textCopy[i] = text[i];
+	}
+
+	return textCopy;
 }
 
 INV_EXPORT void
-INV_CALL overlayelement_set_caption(InvHandle self, _wstring value)
+INV_CALL overlayelement_set_caption(InvHandle self, _string value)
 {
 	Ogre::DisplayString text = value;
 
